2da7b2c3189d2961b90931a27a59f216418d2806
SamCatherman <sam.catherman@gmail.com>
2017-11-14 21:25:46 -0500

    webpack.config, index.html

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// require webpack
+const webpack = require('webpack');
+
+// config object
+let config = {
+  entry: './index.js',
+  output: {
+    filename: 'output.js'
+  }
+}
+
+// export
+module.exports = config;
