8941944a3a12b2a8e4f3777ec1c32be343195029
SamCatherman <sam.catherman@gmail.com>
2017-11-16 20:17:35 -0500

    cleans up directory, adds public and src

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 // require webpack
 const webpack = require('webpack');
+const path = require('path');
 
 // config object
 let config = {
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
+    path: path.resolve(__dirname, './public'),
     filename: 'output.js'
   },
   //specify a loader and a set of rules - webpack finds all js files and compiles them with babel-loader
