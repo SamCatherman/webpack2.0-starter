f3fb4df0ca13cfd6a5ef1133f3920dd39ecb7821
SamCatherman <sam.catherman@gmail.com>
2017-11-18 10:34:51 -0500

    adds webpack-dev-server config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 // require webpack
 const webpack = require('webpack');
 const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
 
 // config object
 let config = {
@@ -20,10 +22,28 @@ let config = {
       //loader for files ending in .scss
       {
         test: /\.scss$/,
-        loader: ['style-loader', 'css-loader', 'sass-loader']
+        //call plugin's extract method
+        use: ExtractTextWebpackPlugin.extract({
+          use: ['css-loader', 'sass-loader'],
+          'fallback': 'style-loader'
+        })
       }
     ]
-  }
+  },
+  //call ExtractTextWebpackPlugin constructor and link css file
+  plugins: [
+    new ExtractTextWebpackPlugin('styles.css')
+  ],
+  //sets directory to serve HTML content from, fallback to index.html on SPAs,
+  //inline mode(set false to disable including client scripts), and open default browser while launching.
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'),
+    historyApiFallback: true,
+    inline: true,
+    open: true
+  },
+  //enable devtool for debugging
+  devtool: 'eval-source-map'
 }
 
 // export
